2204971c2a17e6f892f61e8aa5145c439dbe95a2
Victor Navarro <vn4varro@gmail.com>
2021-01-25 19:18:00 +0100

    feat: create svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+    const endpoint = 'http://localhost:5000';
+
+    let date = new Date()
+    let text = ''
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        const response = await fetch(`${endpoint}?text=${encodeURI(text)}`);
+        if (response.ok) {
+            const dateText = await response.text()
+            date = new Date(dateText)
+        }
+        else {
+            // TODO: Invalid date logic
+        }
+    };
+</script>
+
+<main>
+    <form on:submit={handleSubmit}>
+        <label>
+            Text:
+            <input type="text" bind:value={text}>
+        </label>
+        <button>Submit</button>
+    </form>
+    <pre>{date.toString()}</pre>
+</main>
+
+<style>
+</style>
