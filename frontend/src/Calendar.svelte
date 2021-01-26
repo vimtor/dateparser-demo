0b5e0f3e7f830ea1a7e6929eefb3bebf7158efaf
Victor Navarro <vn4varro@gmail.com>
2021-01-26 19:26:02 +0100

    feat: calendar and date display component

---
+++ frontend/src/Calendar.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let date = new Date()
+    import formatDate from 'dateformat'
+    import calendarize from "calendarize";
+
+    const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    const view = calendarize(date)
+    const days = view.flat()
+</script>
+
+<div>
+    <header>{formatDate(date, 'mmmm yyyy')}</header>
+    <div class="grid" role="list">
+        {#each weekdays as day}
+            <div class="weekday">{day}</div>
+        {/each}
+        {#each days as day}
+            <div class="item" role="listitem" class:selected={date.getDate() === day}>
+                {day ? day : ""}
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(7, 36px);
+    }
+
+    .item {
+        border: 1px solid lightgray;
+        height: 36px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.75rem;
+    }
+
+    .weekday {
+        text-align: center;
+        padding: 0.5em 0;
+        font-size: 0.75rem;
+    }
+
+    .selected {
+        color: white;
+        background-color: hsl(358, 90%, 45%);
+        border-color: hsl(358, 90%, 45%);
+    }
+
+    header {
+        text-align: center;
+        margin-bottom: 0.5em;
+    }
+</style>
---
+++ frontend/src/DateDisplay.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let date = new Date()
+    import formatDate from 'dateformat'
+
+</script>
+
+<div>
+    <h2>{formatDate(date, 'HH:MM')}</h2>
+    <p>{formatDate(date, 'dddd, mmmm dd, yyyy')}</p>
+    <span>{formatDate(date, 'Z')}</span>
+</div>
+
+<style>
+    div {
+        text-align: center;
+        margin: 4rem 0;
+    }
+
+    h2 {
+        font-size: 2rem;
+        margin: 0;
+    }
+
+    p {
+        font-size: 1rem;
+        margin: 1em 0 0;
+    }
+
+    span {
+        display: block;
+        font-size: 0.9rem;
+        margin-top: 0.5em;
+        color: darkgray;
+    }
+</style>
