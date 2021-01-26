6e1c0d3dc6555ca88bcd1cbe92cf42a846b47be8
Victor Navarro <vn4varro@gmail.com>
2021-01-26 19:25:52 +0100

    feat: logo component

---
+++ frontend/src/Logo.svelte
@@ -0,0 +1,9 @@
+<a href="https://dateparser.readthedocs.io/">
+    <img class="logo" src="/images/dateparser-logo.png" alt="dateparser logo">
+</a>
+
+<style>
+    .logo {
+        max-width: 300px;
+    }
+</style>
